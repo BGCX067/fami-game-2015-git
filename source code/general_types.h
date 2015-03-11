b5191956398db54c561c7513bc71de471c005a2f
Maxim <elnadrius@gmail.com>
2015-03-11 16:18:10 +0600

    Добавление 4-х классов команд: Command, CommandWithDirection, CommandAttack, CommandMove

--- source code/general_types.h
+++ source code/general_types.h
@@ -3,3 +3,61 @@
 /*
 Описание типов, общих для нескольких подсистем
 */
+
+
+/*Номер игрока*/
+typedef unsigned int t_player_number;
+
+/*Направление движения/стельбы*/
+enum class Direction {Left, Right, Forward, Backward};
+
+// ========================================================================
+// ======================== Классы команд =================================
+// ========================================================================
+
+/*Базовый класс*/
+class Command {
+ public:
+	 //Конструкторы
+	 Command();
+	 Command(t_player_number s_player_number);
+
+	 //Set-функции
+	 void setPlayerNumber(t_player_number s_player_number);
+
+	 //Get-функции
+	 t_player_number getPlayerNumber();
+
+ private:
+	 t_player_number player_number;
+
+};
+
+/*Команда с направлением */
+class CommandWithDirection : public Command {
+ public:
+	 //Конструкторы
+	 CommandWithDirection();
+	 CommandWithDirection(t_player_number s_player_number, Direction s_command_direction);
+
+	 //Set-функции
+	 void setCommandDirection(Direction s_command_direction);
+
+	//Get-функции
+	 Direction getCommandDirection();
+
+ private:
+	 Direction command_direction;
+};
+
+
+/*Команда движения */
+class CommandMove : public CommandWithDirection {
+
+
+};
+
+/*Команда атаки */
+class CommandAttack : public CommandWithDirection {
+
+};
